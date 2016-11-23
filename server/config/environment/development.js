ed162fcb05eeb73c712842843b35f45afad64b52
Raschlemper <raschlemper@gmail.com>
2016-11-22 23:06:24 -0200

    ajuste google

--- server/config/environment/development.js
+++ server/config/environment/development.js
@@ -7,8 +7,11 @@ module.exports = {
   ip:       '127.0.0.1',
 
   // Server port
-  port:     4000,
+  port:     3000,
   mongo: {
     uri: 'mongodb://localhost/admin-systems'
+  },
+  google: {
+    callbackURL:  'http://localhost:3000/auth/google/callback'
   }
 };
