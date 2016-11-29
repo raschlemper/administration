a42b05cd9c19ce563962117a84153ab211a6d58b
rafael.schlemper <rafael.schlemper@gennera.com.br>
2016-11-29 16:03:04 -0200

    ajustes systems

--- server/auth/passport.js
+++ server/auth/passport.js
@@ -133,7 +133,7 @@ exports.google = function (User, config) {
 // };
 
 var createUser = function(User, profile) {
-  console.log(profile);
+  console.log(profile.image);
   return new User({
     name: profile.displayName,
     email: profile.emails[0].value,
