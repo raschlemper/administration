faf0b1f50fe45eff814bf93344d3f25ee594a557
rafael.schlemper <rafael.schlemper@gennera.com.br>
2016-11-29 22:03:33 -0200

    ajustes systems

--- server/auth/authController.js
+++ server/auth/authController.js
@@ -32,7 +32,7 @@ module.exports = (function () {
   var passportCallback = function(strategy, req, res, next) {
     passport.authenticate(strategy, function (err, user, info) {
       var error = err || (!info || isEmpty(info) ? null : info);
-      console.log('>>>>>>>>>>>>>>>>>>>>>>>>>>>',user);      
+      console.log('>>>>>>>>>>>>>>>>>>>>>>>>>>>', error, user);      
       if (error) return res.json(401, error);
       if (!user) return res.json(404, {message: 'Something went wrong, please try again.'});
       next(user);
