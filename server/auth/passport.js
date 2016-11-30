a671bf6e86ccc89887efd1ed4c693a96996f383f
rafael.schlemper <rafael.schlemper@gennera.com.br>
2016-11-30 08:31:43 -0200

    ajustes systems

--- server/auth/passport.js
+++ server/auth/passport.js
@@ -58,7 +58,7 @@ var saveUser = function(User, userProfile, done, callbackCreateUser) {
   userService.save(userProfile)
     .then(function (result) {
       console.log(result);
-      return done(null, result);
+      return done(null, userProfile);
     }, function(err) {
       return done(err);
     }); 
@@ -68,7 +68,7 @@ var updateUser = function(User, userProfile, done, callbackCreateUser) {
   userService.update(userProfile._id, userProfile)
     .then(function (result) {
       console.log(result);
-      return done(null, result);
+      return done(null, userProfile);
     }, function(err) {
       return done(err);
     }); 
