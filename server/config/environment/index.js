fd9e397d25d7a678b30f0a0da2f8b104381349e6
rafael.schlemper <rafael.schlemper@gennera.com.br>
2016-11-30 08:52:48 -0200

    ajustes systems

--- server/config/environment/index.js
+++ server/config/environment/index.js
@@ -35,7 +35,7 @@ var config = {
 
   google: {
     clientID:     "317866055004-q70qjlt86mu46jkscrqmj1ctpnn4kacv.apps.googleusercontent.com" || 'id',
-    clientSecret: "5_J4UF22rf8aQy_t7pjlZ3Zb" || 'secret',
+    clientSecret: "SfDMnG9xWF88kkjWPiJSKSM2" || 'secret',
     callbackURL:  'https://ras-administration.herokuapp.com/auth/callback/google'
   }
 };
