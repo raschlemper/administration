075c9d53e4d85afa90ad5a52255403d4cc27c183
rafael.schlemper <rafael.schlemper@gennera.com.br>
2016-12-07 15:54:01 -0200

    ajustes

--- server/config/environment/development.js
+++ server/config/environment/development.js
@@ -3,15 +3,9 @@
 // Development Specific Configuration
 // ==================================
 module.exports = {
-  // Server IP
   ip:       '127.0.0.1',
-
-  // Server port
   port:     3000,
   mongo: {
-    uri: 'mongodb://localhost/admin-systems'
-  },
-  google: {
-    callbackURL:  'http://localhost:3000/auth/google/callback'
+    uri: 'mongodb://localhost/administrator'
   }
 };
